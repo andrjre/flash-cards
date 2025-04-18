5378ffc577aeca1121c8e075f56357128247692b
andrjre <andrjre@gmail.com>
2025-04-18 18:02:49 +0100

    Created array system.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let flashcard = document.getElementById("flashcard")
 let flipped = true;
 let next = document.getElementById("next")
-console.log("question")
+let back = document.getElementById("back")
 
 let questions = [
   "What is my favourite color?",
@@ -20,6 +20,8 @@ let answers = [
   "Yes!",
 ]
 
+let pastQuestion = []
+
 let randomIndex = Math.floor(Math.random()* questions.length);
 let randomQuestion = questions[randomIndex]
 let answer = answers[randomIndex]
@@ -27,31 +29,28 @@ let answer = answers[randomIndex]
 flashcard.style.backgroundColor = "white"
 flashcard.style.color = "black"
 flashcard.innerHTML = randomQuestion
-
+pastQuestion.push(flashcard.innerHTML)
 
 
 flashcard.onclick = function(){
     flipped = !flipped;
-
   if (flipped){
-    console.log("question")
     flashcard.style.backgroundColor = "white"
     flashcard.style.color = "black"
     flashcard.innerHTML = randomQuestion
     flashcard.style.transition = "ease-in-out .2s"
     }
     else{
-    console.log("answer")
     flashcard.style.backgroundColor = "grey"
     flashcard.style.color = "white"
     flashcard.innerHTML = answer
     flashcard.style.transition = "ease-in-out .2s"
     }
+
 }
 
+
 next.onclick = function(){
-  console.log("next")
-  console.log("question")
   randomIndex = Math.floor(Math.random()* questions.length);
   randomQuestion = questions[randomIndex]
   answer = answers[randomIndex]
@@ -59,4 +58,11 @@ next.onclick = function(){
   flashcard.style.color = "black"
   flipped = true
   flashcard.innerHTML = randomQuestion
+  console.log(flashcard.innerHTML)
+  pastQuestion.push(flashcard.innerHTML)
+  console.log(pastQuestion)
+}
+
+back.onclick = function(){
+  flashcard.innerHTML = pastQuestion[0]
 }
